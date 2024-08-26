1410f04acc9f6757086a6884e3ed17c98f9a5d0e
IsraelShowell <135654623+IsraelShowell@users.noreply.github.com>
2024-08-26 15:25:09 -0400

    Fixed Project Description

--- Dr-Wiggins-Intel-10-Minute-Question.cpp
+++ Dr-Wiggins-Intel-10-Minute-Question.cpp
@@ -1,10 +1,10 @@
 /*
 Creator: Israel Showell
 Date: 8-26-2024
-Project Name: Dr.Wiggins' 10 - Minute Paper Intel Interview Question
+Project Name: Dr.Wiggins' Intel Question
 Description:
-Dr.Wiggins' told us about his Intel interview, and I figured it out and wrote down the solution in 5 minutes on paper.
-The question was how many cycles does it take to count to 100 by 3's, and every 5th cycle, subtract 8.
+Dr.Wiggins' told us about his 10 minute Intel interview question, and I figured it out and wrote down the solution in 5 minutes on paper. 
+The question was how many cycles does it take to count to 100 by 3's, and every 5th cycle, subtract 8. Dr.Wiggins did his in C, and I used C++. 
 This is the code version of the program for my records.
 */
 
